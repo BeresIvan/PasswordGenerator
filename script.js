eaca823f52e62105172ed1f147081df8b73d6b9e
GTIvan <ivberes@gmail.com>
2020-10-17 16:27:27 -0500

    changed java script functions and html code

--- script.js
+++ script.js
@@ -1,87 +1,43 @@
-
-let passwordEl = document.getElementById("password");
-let lengthEl = document.getElementById("length");
-let lowercaseEl = document.getElementById("lovercase");
-let uppercaseEl = document.getElementById("uppercase");
-let numbersEl = document.getElementById("numbers");
-let symbolsEl = document.getElementById("symbols");
-let generateEl = document.getElementById("generate");
+let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+let uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let specialCharacters = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", "|", "\\", ";", "'", ".", ",", "?", "~", "`", "+", "_", "-",]
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
 
 
-const randomFunc = {
-  lower: getRandomlowerCase,
-  upper: getRandomupperCase,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
-// generate event listener
-generateEl.addEventListener("click", () => {
-  let length = +lengthEl.value;
-  let hasLower = lowercaseEl.checked;
-  let hasUpper = uppercaseEl.checked;
-  let hasNumber = numbersEl.checked;
-  let hasSymbol = symbolsEl.checked;
-
-  resultEl.innerText = generatePassword(
-    length,
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol
-  );
-});
-// Genearate Password function
-function generatePassword(length, lower, upper, number, symbol) {
-  // Initiate password variable
-  let generatedPassword = " ";
-  // filter out unused characters
-  let typesCount = lower + upper + number + symbol;
-
-  console.log("typesCount: ", typesCount);
-
-  let typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
-    (item => Object.values(item)[0]
-  );
-
-  if (typesCount === 0) {
-    return " ";
-  }
-
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      let funcName = Object.keys(type)[0];
-
-      generatedPassword += randomFunc[funcName]();
-    });
+function generatePassword() {
+  let passwordLength = prompt("Please enter pw length between 8 and 128");
+  let includeLowercaseLetters = confirm("Do you want lowercase letters?");
+  let includeUppercaseLetters = confirm("Do you want uppercase letters?");
+  let includeNumbers = confirm("Do you want Numbers letters?");
+  let includeSymbols = confirm("Do you want Symbols letters?");
   
-  }
-
-  return finalPassword;
 
 
 
+    let password = [];
+    for (let i=0; i < passwordLength; 1++){
+    }
+    return password;
+}
 
+generatePassword();
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
-//lowercase randomization
-function getRandomlowerCase() {
-  let lowerCase = "abcdefghijklmnopqrstuvxyz";
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-//upercase randomization
-function getRandomupperCase() {
-  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-function getRandomNumber() {
-  let numbers = "0123456789";
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function getRandomSymbol() {
-  let symbols = " !@#$%^&*(){}[]=<>/|\;'.,?~`+_-";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+let lowercaseLetters = ["A", "B", "C"]
+let numbers = ["1", "2", "3"]
+let specialCharacters = ["&", "(", "%"]
 
